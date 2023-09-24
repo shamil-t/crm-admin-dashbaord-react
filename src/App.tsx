2d526571ea79783c608431c437ca5333853dcbf4
shamil-t <tshamil90@gmail.com>
2023-09-24 11:06:22 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {DashboardPage} from "./pages/DashboardPage.tsx";
+
+function App() {
+
+    return (
+        <div className="h-[100vh] bg-white">
+            <DashboardPage/>
+        </div>
+    )
+}
+
+export default App
